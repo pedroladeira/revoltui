c060c2bccd887bae2ea6c69c157f6a7232984456
Pedro Ladeira <pedroladeira@be-innovation.pt>
2020-01-09 22:09:09 +0000

    update menus documentation

--- src/views/pages/components/Menus.vue
+++ src/views/pages/components/Menus.vue
@@ -1,46 +1,48 @@
 <template>
-  <DocContent title="Menus" id="menus">
-    <div slot="example" class="mb-5">
-      <div class="row">
-        <div class="col-sm-3">
-          <v-menu title="NavMenu">
-            <v-menu-item>Homepage</v-menu-item>
-            <v-menu-item>Item One</v-menu-item>
-            <v-menu-item to="https://www.google.com" target="_blank">
-              Item Two
-            </v-menu-item>
-          </v-menu>
+  <div>
+    <div class="m-3">
+      <v-header>Menus</v-header>
+    </div>
+    <DocContent title="Menus" >
+      <div slot="example" class="mb-5">
+        <div class="row">
+          <div class="col-sm-3">
+            <v-menu title="NavMenu">
+              <v-menu-item>Homepage</v-menu-item>
+              <v-menu-item>Item One</v-menu-item>
+              <v-menu-item to="https://www.google.com" target="_blank">
+                Item Two
+              </v-menu-item>
+            </v-menu>
+          </div>
+        </div>
+        <div class="row mt-2">
+          <div class="col-sm-12">
+            <v-menu title="Horizontal Menu" view="horizontal">
+              <v-menu-item>Homepage</v-menu-item>
+              <v-menu-item>Item One</v-menu-item>
+              <v-menu-item to="https://www.google.com" target="_blank">
+                Item Two
+              </v-menu-item>
+            </v-menu>
+          </div>
         </div>
       </div>
-      <div class="row mt-2">
-        <div class="col-sm-12">
-          <v-menu title="Horizontal Menu" view="horizontal">
-            <v-menu-item>Homepage</v-menu-item>
-            <v-menu-item>Item One</v-menu-item>
-            <v-menu-item to="https://www.google.com" target="_blank">
-              Item Two
-            </v-menu-item>
-          </v-menu>
-        </div>
-      </div>
-    </div>
-    <template slot="code" type="html">
-      <code class="html">
-        {{ code }}
-      </code>
-    </template>
-    <div class="footer">
-      <p>
-        <strong>(slot)</strong>
-      </p>
-      <p class="text-sm">
-        string
-      </p>
-      <p>
-        <strong>:view</strong> can be <i>horizontal</i>
-      </p>
-    </div>
-  </DocContent>
+      <template slot="code" type="html">
+        <code class="html">
+          {{ code }}
+        </code>
+      </template>
+
+      <TablePropsDocsItem slot="doc-props">
+        <template slot="name">view</template>
+        <template slot="type">string</template>
+        <template slot="default">vertical</template>
+        <template slot="mandatory"></template>
+        <template slot="description">vertical or horizontal</template>
+      </TablePropsDocsItem>
+    </DocContent>
+  </div>
 </template>
 
 <script>
