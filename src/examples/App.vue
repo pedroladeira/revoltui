aa6d3915f87d96694db393d63d75b21cc54631c9
Pedro Ladeira <pedr.ladeira@gmail.com>
2020-01-15 14:18:21 +0000

    update new structure

--- src/examples/App.vue
+++ src/examples/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
     <ul>
-      <li>
-        <router-link to="/buttons">Buttons</router-link>
-      </li>
-      <li>
-        <router-link to="/badges">Badges</router-link>
+      <li v-for="(r, i) in routes" :key="i">
+        <router-link :to="r.path">{{ r.name }}</router-link>
       </li>
     </ul>
+    <hr />
     <div class="mt-6">
       <router-view />
     </div>
   </div>
 </template>
+
+<script>
+import Router from "./router.js";
+
+export default {
+  computed: {
+    routes: () => {
+      return Router.options.routes;
+    }
+  }
+};
+</script>
--- src/examples/router.js
+++ src/examples/router.js
@@ -2,17 +2,25 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Buttons from "@/examples/Buttons";
 import Badges from "@/examples/Badges";
+import Cards from "@/examples/Cards";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
+    name: "Buttons",
     path: "/buttons",
     component: Buttons
   },
   {
+    name: "Badges",
     path: "/badges",
     component: Badges
+  },
+  {
+    name: "Cards",
+    path: "/cards",
+    component: Cards
   }
 ];
 
